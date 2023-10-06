bea566e1bf67e8f29f8a92aa7be0d595acfea30a
Josep Fornies <josep.fornies@bonhams.com>
2023-10-06 09:55:20 +0200

    fix: update extension build

--- apps/downloader-extension/src/app/app.tsx
+++ apps/downloader-extension/src/app/app.tsx
@@ -1,15 +1,12 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { Link, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 
-import Action from './action/action'
-import styles from './app.module.scss'
 import NxWelcome from './nx-welcome'
 
 export function App() {
   return (
     <Routes>
       <Route path="/" element={<div>This is the application root</div>} />
-      <Route path="/action/*" element={<Action />} />
+      <Route path="/action/*" element={<NxWelcome title="sample" />} />
     </Routes>
   )
 }
--- apps/downloader-extension/webpack.config.js
+++ apps/downloader-extension/webpack.config.js
@@ -1,9 +1,25 @@
 const { composePlugins, withNx } = require('@nx/webpack')
 const { withReact } = require('@nx/react')
+const path = require('node:path')
+const fs = require('node:fs')
+const GeneroutedPlugin = require('./plugin')
 
-// Nx plugins for webpack.
+const backgroundFile = path.join(__dirname, 'src/background/index.ts')
+const hasBackground = fs.existsSync(backgroundFile)
+
+const stylesFile = path.join(__dirname, 'src/styles.scss')
+const hasStyles = fs.existsSync(stylesFile)
+
+const mainFile = path.join(__dirname, 'src/main.tsx')
+const hasMain = fs.existsSync(mainFile)
+
 module.exports = composePlugins(withNx(), withReact(), (config) => {
-  // Update the webpack config as needed here.
-  // e.g. `config.plugins.push(new MyPlugin())`
+  config.plugins.push(new GeneroutedPlugin())
+  config.entry = {
+    ...(hasMain && { main: [mainFile] }),
+    ...(hasStyles && { styles: [stylesFile] }),
+    ...(hasBackground && { background: [backgroundFile] }),
+  }
+
   return config
 })
